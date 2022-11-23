daacac438aca303dd8facf7275340acd2700024c
SiwonHwang <sight41siyah@gmail.com>
2022-11-23 22:16:08 +0900

    build structure

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +17,37 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // get all the movies in the movie list
+  @Get()
+  getAllMovies(): string {
+    return this.appService.getMovies();
+  }
+
+  // get a movie in the movie list with the id
+  @Get(':id')
+  getMovieById(@Param('id') id: string): string {
+    return this.appService.getOneMovie(id);
+  }
+
+  // remove a movie in the movie list with the id
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string): string {
+    return this.appService.remove(id);
+  }
+
+  // update a movie data in the movie list with the id
+  @Patch(':id')
+  updateMovie(@Param('id') id: string): string {
+    return this.appService.update(id);
+  }
+
+  // save a movie data in the movie list with the id
+  // should add a @Body decorator for get informations about movie
+  // + should add dto to add ⬆️
+  @Post()
+  createMovie(): string {
+    return this.appService.create();
+  }
+  //movie 폴더를 따로 만들어야할듯
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,29 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  //movies: string[] = [];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getMovies(): string {
+    return 'This will return movie list';
+  }
+
+  getOneMovie(id): string {
+    return `This will return a movie with id : ${id}`;
+  }
+
+  remove(id): string {
+    return `This will delete a movie with id : ${id}`;
+  }
+
+  update(id): string {
+    return `This will update a movie data with id : ${id}`;
+  }
+
+  create(): string {
+    return 'This will create a movie data in the list';
   }
 }
